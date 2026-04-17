443c04540511f463efd67aa9d039079c438c030e
mst-mkt <mail@keito.dev>
2026-04-17 21:20:27 +0900

    fix: regenerate migration for contestVoteNumber

--- packages/infrastructure/src/db/migrations/0002_useful_thena.sql
+++ packages/infrastructure/src/db/migrations/0002_useful_thena.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `projects` ADD `contest_vote_number` integer;--> statement-breakpoint
+ALTER TABLE `projects` ADD `contest_vote_number` text;--> statement-breakpoint
 CREATE UNIQUE INDEX `projects_event_vote_number_unique` ON `projects` (`event_id`,`contest_vote_number`);
